fb81652064665a8d23e37dcda8db2be9ddaaf809
Sherif Ibrahim <ibrahimsherifdeji@gmail.com>
2026-06-17 16:20:44 +0100

    add dynamic greeting route

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Welcome to my Hello FastAPI app!"}
+
+@app.get("/greet/{name}")
+async def greet_user(name: str):
+    return{"greeting": f"Hello there, {name}! Nice to have you here..."}
